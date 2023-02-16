d558b276fc70828dbaeac0e608dc37af1c0139d2
Vasil Vertyporokh <vasil.vertiporokh@chisw.com>
2023-02-16 16:39:49 +0100

    added network layer

--- BuySellCarApp/Application/Configuration/AppConfiguration.swift
+++ BuySellCarApp/Application/Configuration/AppConfiguration.swift
@@ -13,14 +13,32 @@ protocol AppConfiguration {
 }
 
 final class AppConfigurationImpl: AppConfiguration {
+    // MARK: - Internal properties
     let bundleId: String
     let environment: AppEnvironment
+
+    var baseUrl: URL {
+        guard let url = URL(string: apiUrl) else {
+            fatalError("Invalid url")
+        }
+        let fullUrl = url.appendingPathComponent(appId).appendingPathComponent(apiKey)
+        return fullUrl
+    }
     
+    // MARK: - Private properties
+    private let apiKey: String
+    private let appId: String
+    private let apiUrl: String
+    
+    // MARK: - Init
     init(bundle: Bundle = .main) {
         guard
             let bundleId = bundle.bundleIdentifier,
             let infoDict = bundle.infoDictionary,
             let environmentValue = infoDict[Key.Environment] as? String,
+            let apiKey = infoDict[Key.ApiKey] as? String,
+            let appId = infoDict[Key.AppId] as? String,
+            let apiUrl = infoDict[Key.BaseUrl] as? String,
             let environment = AppEnvironment(rawValue: environmentValue)
         else {
             fatalError("config file error")
@@ -28,12 +46,19 @@ final class AppConfigurationImpl: AppConfiguration {
         
         self.bundleId = bundleId
         self.environment = environment
+        self.apiKey = apiKey
+        self.appId = appId
+        self.apiUrl = apiUrl
 
         debugPrint(environment)
         debugPrint(bundleId)
+        debugPrint("⚙️ \(baseUrl)")
     }
 }
 
 fileprivate enum Key {
     static let Environment: String = "APP_ENVIRONMENT"
+    static let ApiKey: String = "API_KEY"
+    static let AppId: String = "APP_ID"
+    static let BaseUrl: String = "BASE_URL"
 }
---
+++ BuySellCarApp/BusinessLogic/Services/Networking/HTTPMethods.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPMethods.swift
+//  BuySellCarApp
+//
+//  Created by Vasil Vertiporokh on 16.02.2023.
+//
+
+import Foundation
+
+// MARK: - HTTPMethod
+enum HTTPMethod: String {
+    case options = "OPTIONS"
+    case delete = "DELETE"
+    case patch = "PATCH"
+    case post = "POST"
+    case get = "GET"
+    case put = "PUT"
+}
